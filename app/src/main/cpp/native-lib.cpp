6d88811501f49ba405090dd40a5868a3d0dd06fa
DKU-PC <ah9zb82a@gmail.com>
2024-05-23 14:41:08 +0900

    TODO: Parse /proc/bus/input/devices to get fpga-keypad device event file.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,4 @@
 #include <jni.h>
-#include <string>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -13,56 +12,58 @@
 #include <string.h>
 #include <stdio.h>
 #include <sys/time.h>
+#include <errno.h>
 
-struct input_event {
+typedef struct input_event {
     struct timeval time;
     unsigned short type;
     unsigned short code;
     unsigned int value;
-};
+} input_event;
 
-extern "C" JNIEXPORT jstring
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
-JNICALL
-Java_com_example_intmob_MainActivity_stringFromJNI(
+JNIEXPORT jstring JNICALL Java_com_example_intmob_MainActivity_stringFromJNI(
         JNIEnv *env,
-        jobject /* this */) {
+        jobject mainActivity) {
     int fd;
-    int ret;
-    char path[] = "/dev/input/event4";
-    struct pollfd fds[1];
-    fds[0].fd = open(path, O_RDONLY | O_NONBLOCK);
-    if (fds[0].fd < 0) {
-        return env->NewStringUTF("error unable open for reading");
+    const char *EVENT;
+
+    EVENT = "/dev/input/event4";
+
+    fd = open(EVENT, O_RDONLY);
+
+    if (fd < 0) {
+        char* errmsg = strerror(errno);
+        return env->NewStringUTF(errmsg);
     }
 
-    const int input_size = sizeof(struct input_event);
-    const int timeout_ms = -1;
-    struct input_event *input_data;
-    memset(input_data, 0, input_size);
-    fds[0].events = POLLIN;
+    const int input_size = sizeof(input_event);
 
+    input_event input_data;
 
-    ret = poll(fds, 1, timeout_ms);
-    if (ret < 0) {
-        return env->NewStringUTF("timeout\n");
-
-    }
-
-    if (!fds[0].revents) {
-        return env->NewStringUTF("error\n");
-
-    }
-
-    ssize_t r = read(fds[0].fd, input_data, input_size);
+    ssize_t r = read(fd, &input_data, input_size);
 
     if (r < 0) {
-        return env->NewStringUTF("error");
+        char* errmsg = strerror(errno);
+        return env->NewStringUTF(errmsg);
     }
 
-    printf("time=%ld.%06lu type=%hu code=%hu value=%u\n", input_data->time.tv_sec,
-           input_data->time.tv_usec, input_data->type, input_data->code, input_data->value);
+    //printf("time=%ld.%06lu type=%hu code=%hu value=%u\n", input_data->time.tv_sec,input_data->time.tv_usec, input_data->type, input_data->code, input_data->value);
+
     char result[1];
-    result[0] = '0' + (input_data->code-1);
+    result[0] = '0' + (input_data.code - 1);
     return env->NewStringUTF(result);
 }
+
+JNIEXPORT void JNICALL Java_com_example_intmob_MainActivity_div0(JNIEnv *env, jobject mainActivity){
+    int i = 0;
+    i /= i;
+}
+
+#ifdef __cplusplus
+}
+#endif
--- app/src/main/java/com/example/intmob/MainActivity.java
+++ app/src/main/java/com/example/intmob/MainActivity.java
@@ -2,20 +2,28 @@ package com.example.intmob;
 
 import static java.lang.Thread.sleep;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
 import android.os.Message;
-import android.view.KeyEvent;
 import android.widget.TextView;
 
 import com.example.intmob.databinding.ActivityMainBinding;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFileAttributes;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Set;
+import java.nio.file.*;
+import java.nio.file.attribute.*;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,62 +33,77 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private ActivityMainBinding binding;
-
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        boolean ret = super.onKeyDown(keyCode, event);
-
-        TextView tv = binding.sampleText;
-        //tv.setText(String.valueOf(keyCode));
-
-
-        return ret;
-    }
-
-    MyThread m_thread;
-    EventHandler m_eventHandler;
+    private MyThread m_thread;
+    private SetTextHandler setTextHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+
         setContentView(binding.getRoot());
 
-        UpdateValue();
+        setTextHandler = new SetTextHandler();
 
-        m_eventHandler = new EventHandler();
+        setText(getFileType("/proc/bus/input/devices"));
 
         m_thread = new MyThread();
         m_thread.start();
     }
 
-    class MyThread extends Thread
+    private void setText(Object text){
+        Message msg1 = setTextHandler.obtainMessage(0, text);
+        setTextHandler.sendMessage(msg1);
+    }
+
+    private static String getFileType(String path){
+        File file = new File(path);
+        if(file.isFile()){
+            return "file";
+        }
+        if(file.isDirectory()){
+            return "directory";
+        }
+        if(file.exists()){
+            return "exists";
+        }
+        return "does not exist";
+    }
+
+    private static void exec(String command) throws IOException {
+        Runtime.getRuntime().exec(command);
+    }
+
+    private class MyThread extends Thread
     {
         public void run(){
             try{
+                Thread.sleep(1000);
+                exec("chmod 777 /dev/input/*");
+                setText(1234);
                 while(true){
-                    Thread.sleep(100);
-                    Message msg1 = m_eventHandler.obtainMessage();
-                    m_eventHandler.sendMessage(msg1);
+
+                    Thread.sleep(1000);
+                    Message msg1 = setTextHandler.obtainMessage(0, stringFromJNI());
+                    setTextHandler.sendMessage(msg1);
                 }
             }
             catch(Exception e){
-                System.out.println(e);
+                System.err.println(e);
             }
         }
     }
 
-    public void UpdateValue(){
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
-    }
 
-    class EventHandler extends Handler
+    private class SetTextHandler extends Handler
     {
-        EventHandler(){}
-        public void handleMessage(Message msg){
-            UpdateValue();
+        public void handleMessage(Message msg ){
+            TextView v = binding.sampleText;
+            Object obj = msg.obj;
+            String sobj = obj.toString();
+            v.setText(sobj);
         }
     }
 
@@ -88,5 +111,6 @@ public class MainActivity extends AppCompatActivity {
      * A native method that is implemented by the 'intmob' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    private native String stringFromJNI();
+    private native void div0();
 }
